71c175056d62596bc616f10b0693a38e1fa81f74
zxq <3235358845@qq.com>
2025-06-30 16:17:28 +0800

    优化移动端界面

--- app/src/main/java/com/rainkaze/traffic/adapter/WarningAdapter.java
+++ app/src/main/java/com/rainkaze/traffic/adapter/WarningAdapter.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat; // 引入ContextCompat
 import androidx.recyclerview.widget.RecyclerView;
 import com.rainkaze.traffic.R;
 import com.rainkaze.traffic.model.DashboardDataDto;
@@ -35,15 +36,18 @@ public class WarningAdapter extends RecyclerView.Adapter<WarningAdapter.WarningV
         holder.plateNumber.setText(warning.getPlateNumber());
         holder.violationType.setText(warning.getViolationType());
         holder.location.setText(warning.getLocation());
-        holder.timeAgo.setText(warning.getTimeAgo());
+        // *** 核心修改点 3 ***
+        // holder.timeAgo.setText(warning.getTimeAgo()); // 此行已被删除
 
         // 根据告警级别设置文本和颜色
         if (warning.getWarningLevel() == 1) {
             holder.warningLevel.setText("高危");
-            holder.warningLevel.setTextColor(Color.parseColor("#F44336")); // Red
+            // 使用我们预定义的颜色，而非硬编码
+            holder.warningLevel.setTextColor(ContextCompat.getColor(context, R.color.status_danger));
         } else {
             holder.warningLevel.setText("普通");
-            holder.warningLevel.setTextColor(Color.parseColor("#FF9800")); // Orange
+            // 使用我们预定义的颜色
+            holder.warningLevel.setTextColor(ContextCompat.getColor(context, R.color.status_warning));
         }
     }
 
@@ -58,14 +62,17 @@ public class WarningAdapter extends RecyclerView.Adapter<WarningAdapter.WarningV
     }
 
     static class WarningViewHolder extends RecyclerView.ViewHolder {
-        TextView plateNumber, violationType, location, timeAgo, warningLevel;
+        // *** 核心修改点 1 ***
+        // 移除了 timeAgo 的声明
+        TextView plateNumber, violationType, location, warningLevel;
 
         public WarningViewHolder(@NonNull View itemView) {
             super(itemView);
             plateNumber = itemView.findViewById(R.id.textViewPlateNumber);
             violationType = itemView.findViewById(R.id.textViewViolationType);
             location = itemView.findViewById(R.id.textViewLocation);
-            timeAgo = itemView.findViewById(R.id.textViewTimeAgo);
+            // *** 核心修改点 2 ***
+            // 移除了 findViewById 对 R.id.textViewTimeAgo 的调用
             warningLevel = itemView.findViewById(R.id.textViewWarningLevel);
         }
     }
